be93f77e75a83bedfda11c6d7105c6c8bfd0d46d
bgdntol <whykitto@gmail.com>
2021-03-02 23:18:53 +0200

    Created project Added basic styles and markup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+
+import './fonts/Epilogue-Bold.ttf';
+import './fonts/Epilogue-Bold.woff';
+import './fonts/Epilogue-Bold.woff2';
+import './fonts/Epilogue-Regular.ttf';
+import './fonts/Epilogue-Regular.woff';
+import './fonts/Epilogue-Regular.woff2';
+import './fonts/OpenSans-Bold.ttf';
+import './fonts/OpenSans-Bold.woff';
+import './fonts/OpenSans-Bold.woff2';
+import './fonts/OpenSans-Regular.ttf';
+import './fonts/OpenSans-Regular.woff';
+import './fonts/OpenSans-Regular.woff2';
+
+import './index.html';
+import './index.scss';
+import './scripts/script';
+
+OfflinePluginRuntime.install();
