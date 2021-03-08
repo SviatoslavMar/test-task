1a266c220e1b0750fb7f2d3b0c67c77d896deefe
bgdntol <whykitto@gmail.com>
2021-03-08 11:37:46 +0200

    Added dist folder

--- src/scripts/module/glide.js
+++ src/scripts/module/glide.js
@@ -2,9 +2,14 @@ import Glide, { Controls, Breakpoints, Swipe} from '@glidejs/glide'
 
 const glideSlider = new Glide('.glide', {
   type: 'carousel',
-  autoplay: 3500,
   perView: 3,
-  gap: 0,
+  autoplay: 3500,
+  focusAt: 'center',
+  breakpoints: {
+    480: {
+      perView: 1
+    },
+  }
 }).mount();
 
 export default glideSlider;
